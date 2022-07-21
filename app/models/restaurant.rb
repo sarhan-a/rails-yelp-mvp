309c241400d6f9656a6420c28b766b19d8b5ba71
Sarhan Alim <sarhan.alim@gmail.com>
2022-07-21 20:57:11 +0100

    add models validation and associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates_presence_of :name, :address, :category
+  validates :category, inclusion: { in: CATEGORIES }
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates_presence_of :rating, :content
+  validates_inclusion_of :rating, :in => 0..5
+  validates :rating, numericality: { only_integer: true }
 end
